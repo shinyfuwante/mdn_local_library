d81bf9d711b0ef3bf57fa374ad26a629a5a42d43
shinyfuwante <mr.philliptang@gmail.com>
2023-03-12 20:09:46 -0700

    Finish most of tutorial, do bookinstance delete

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const catalogRouter = require("./routes/catalog");
+
 require('dotenv').config();
 var app = express();
 const mongoose = require("mongoose");
@@ -29,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- models/author.js
+++ models/author.js
@@ -1,25 +1,19 @@
 const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
 
 const Schema = mongoose.Schema;
 
 const AuthorSchema = new Schema({
-  first_name: {
-    type: String,
-    required: true,
-    maxLength: 100,
-  },
-  family_name: {
-    type: String,
-    required: true,
-    maxLength: 100,
-  },
-  date_of_birth: {
-    type: Date,
-  },
+  first_name: { type: String, required: true, maxLength: 100 },
+  family_name: { type: String, required: true, maxLength: 100 },
+  date_of_birth: { type: Date },
   date_of_death: { type: Date },
 });
 
+// Virtual for author's full name
 AuthorSchema.virtual("name").get(function () {
+  // To avoid errors in cases where an author does not have either a family name or first name
+  // We want to make sure we handle the exception by returning an empty string for that case
   let fullname = "";
   if (this.first_name && this.family_name) {
     fullname = `${this.family_name}, ${this.first_name}`;
@@ -30,4 +24,21 @@ AuthorSchema.virtual("name").get(function () {
   return fullname;
 });
 
+// Virtual for author's URL
+AuthorSchema.virtual("url").get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return `/catalog/author/${this._id}`;
+});
+
+AuthorSchema.virtual("lifespan").get(function () {
+  const birth_date = DateTime.fromJSDate(this.date_of_birth).toLocaleString(
+    DateTime.DATE_MED
+  );
+  const death_date = DateTime.fromJSDate(this.date_of_death).toLocaleString(
+    DateTime.DATE_MED
+  );
+  return `${birth_date} - ${death_date}`;
+});
+
+// Export model
 module.exports = mongoose.model("Author", AuthorSchema);
